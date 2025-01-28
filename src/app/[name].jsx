1b49df1f0add8a89514ec4ce672a4d0ee50e53c7
areeb193 <fa22-bse-193@cuilahore.edu.pk>
2025-01-28 22:11:09 +0500

    see more or see less

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -2,8 +2,11 @@ import {View , Text ,StyleSheet,ScrollView} from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import exercises from '../../assets/data/exercises';
 import {Stack} from 'expo-router';
+import {useState} from 'react';
 export default function ExerciseDetailScreen() {
   const params = useLocalSearchParams();
+  const[instructionsExpanded,setInstructionsExpanded]=useState(false);
+  
   const exercise = exercises.find((item) => item.name === params.name);
   if (!exercise) {
     return <Text>Exercise not found</Text>;
@@ -21,7 +24,9 @@ export default function ExerciseDetailScreen() {
            </Text>
            </View>
            <View style={styles.panel}>
-           <Text  style={styles.instructions}>{exercise.instructions}</Text>
+           <Text  style={styles.instructions} numberOfLines={instructionsExpanded ? 0:3}>{exercise.instructions}</Text>
+           <Text onPress={()=>setInstructionsExpanded(!instructionsExpanded)
+           } style={styles.seeMore}>{instructionsExpanded ? 'See less': 'See more'}</Text>
            </View>
     </ScrollView>
   );
@@ -52,6 +57,12 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     
+  },
+  seeMore:{
+    color:'blue',
+    alignSelf:'center',
+    fontWeight:'600',
+    padding : 5,
   }
   
 });
